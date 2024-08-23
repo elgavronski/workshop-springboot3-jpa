225bfcd80b3ccd442277aac9275610fc03d1aeae
Ellen Gavronski <ellengavronski.el@hotmail.com>
2024-08-23 15:24:11 -0300

    Service layer, component registration

--- src/main/java/com/eduacandoweb/course/entities/User.java
+++ src/main/java/com/eduacandoweb/course/entities/User.java
@@ -1,10 +1,8 @@
 package com.eduacandoweb.course.entities;
 
 import java.io.Serializable;
-import java.util.Objects;
 import jakarta.persistence.Entity;
 
-import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
